67933914d440dcf21f64d30d59f970923c70f1fa
Basha <basha502@gmail.com>
2019-08-05 18:51:09 +0530

    refactored and cleaned up code

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,8 @@ const routes = express.Router();
 const User = require("../model/emp");
 
 routes.get("/list", async (req, res) => {
-  // console.log(User);
-  //  User.findAll().then(users => {
-  //   let data = JSON.stringify(users, null, 4);
-
-  //   console.log("All users:", JSON.stringify(users, null, 4));
-
-  // });
-  let data = await User.findAll();
-  res.send(data);
+  const users = await User.findAll();
+  res.send(users);
 });
 
 module.exports = routes;
